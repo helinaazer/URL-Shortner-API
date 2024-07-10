b6044eb72167060da685b21a8ed27e6bc832f4c7
Helina Azer <hazer@uw.edu>
2024-07-10 11:41:22 -0700

    implementing auth3

--- urlShortner/Controllers/navigateController.cs
+++ urlShortner/Controllers/navigateController.cs
@@ -1,11 +1,11 @@
-// using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace UrlShortner.Controllers;
 
-// [Authorize(AuthenticationSchemes = $"{JwtBearerDefaults.AuthenticationScheme},BasicAuthentication")]
-// [ApiController]
+[Authorize(AuthenticationSchemes = $"{JwtBearerDefaults.AuthenticationScheme},BasicAuthentication")]
+[ApiController]
 [Route("[controller]")]
 public class NavigateController : ControllerBase
 {
